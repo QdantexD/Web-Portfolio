7bb77db14779f3c25bfe4458253b618d278e70c1
Andree <pendejoga69@gmail.com>
2024-04-24 18:24:58 -0500

    Cambios del nombres

--- web/Mis_avances_portafolio/main.js
+++
@@ -1,7 +0,0 @@
-let menu = document.querySelectorAll("#menu-bars");
-let navbar = document.querySelector(".navbar");
-
-menu.onclick = () => {
-  menu.classList.toggle("fa-time");
-  navbar.classList.toggle("active");
-};
